628bfefdf1a1b1c88a989bc2d3bddb3eb2ebcb66
Rahul <91822100+MRaahul-04@users.noreply.github.com>
2024-09-19 01:53:01 +0530

    Initial commit

--- __init__.py
+++ __init__.py
@@ -14,8 +14,8 @@ db = SQLAlchemy(app)
 
 pymysql.install_as_MySQLdb()
 
-# Import routes after app and db initialization to avoid circular imports
-# from routes import *
+# Import routes after an app and db initialization to avoid circular imports
+from routes import *
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
